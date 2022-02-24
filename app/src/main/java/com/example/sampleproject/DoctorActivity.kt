c36a42f91170d07e5e69b47061ec6c09645b5bbe
niha <niha8515@yahoo.com>
2022-02-23 23:44:14 -0800

    Initial commit

---
+++ app/src/main/java/com/example/sampleproject/DoctorActivity.kt
@@ -0,0 +1,16 @@
+package com.example.sampleproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sampleproject.databinding.ActivityDoctorBinding
+
+class DoctorActivity : AppCompatActivity() {
+    lateinit var binding: ActivityDoctorBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityDoctorBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        
+    }
+}
