c8eefd7d007d84822727c9b184675d55fff35c5b
niha <niha8515@yahoo.com>
2022-02-24 04:15:11 -0800

    Add Doctor.kt and show consultancy

--- app/src/main/java/com/example/sampleproject/Doctor.kt
+++ app/src/main/java/com/example/sampleproject/Doctor.kt
@@ -1,4 +1,39 @@
 package com.example.sampleproject
 
-data class Doctor(val id :Int ) {
+import java.time.LocalDateTime
+
+object Hospital{
+    val doctorList = arrayListOf<Doctor>()
+    val consultancyList = arrayListOf(
+        Consulatancy(1,12,ConsultancyType.Phone,25000),
+        Consulatancy(2,14,ConsultancyType.Video,24000),
+        Consulatancy(3,17,ConsultancyType.Phone,25000)
+    )
+
+    fun setTestDate(){
+        doctorList.clear()
+        doctorList.add(Doctor(1 , "Dr. sara" ,  OnlineStatus.Offline , "روانشناسی و مشاوره" ,R.drawable.doctor_icon))
+        doctorList.add(Doctor(2 , "Dr. rostam" , OnlineStatus.Online , "روانشناسی و مشاوره" ))
+    }
+}
+
+
+
+data class Doctor(val id :Int ,
+val name :String,
+var onlineStatus :OnlineStatus,
+val field:String ,
+var imageId:Int? = null) {
+}
+
+data class Consulatancy (val id: Int,
+val time:Int ,
+val type :ConsultancyType ,
+var price :Int){}
+
+enum class ConsultancyType{
+    Phone , Video
+}
+enum class OnlineStatus{
+    Online, Offline
 }
