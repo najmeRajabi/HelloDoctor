24773470feb3614e1cb2248ab823f00374f122ee
niha8515 <niha8515@yahoo.com>
2022-03-12 16:06:20 +0330

    add viewModel dependency

--- app/src/main/java/com/example/sampleproject/DoctorListActivity.kt
+++ app/src/main/java/com/example/sampleproject/DoctorListActivity.kt
@@ -2,8 +2,10 @@ package com.example.sampleproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 
 class DoctorListActivity : AppCompatActivity() {
+    val viewModelListDr :DoctorListViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_doctor_list)
