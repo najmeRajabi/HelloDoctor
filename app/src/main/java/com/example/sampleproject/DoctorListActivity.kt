dc447458b6a881049d442c2cfbd89c85a9ab6529
niha8515 <niha8515@yahoo.com>
2022-03-12 15:18:03 +0330

    add DoctorListActivity.kt

---
+++ app/src/main/java/com/example/sampleproject/DoctorListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sampleproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DoctorListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_doctor_list)
+    }
+}
