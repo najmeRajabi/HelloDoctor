4eb4f27a1c37396a4e26077f7d62a82135f1d5c7
niha8515 <niha8515@yahoo.com>
2022-04-21 12:14:06 +0430

    update DoctorAdapter.kt by ImageView

--- app/src/main/java/com/example/sampleproject/DoctorAdapter.kt
+++ app/src/main/java/com/example/sampleproject/DoctorAdapter.kt
@@ -3,6 +3,7 @@ package com.example.sampleproject
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
@@ -11,9 +12,11 @@ RecyclerView.Adapter<DoctorAdapter.ViewHolder>(){
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView
+        val imageView: ImageView
 
         init {
             textView = view.findViewById(R.id.txv_list_row)
+            imageView = view.findViewById(R.id.img_doctor)
         }
     }
 
@@ -26,6 +29,7 @@ RecyclerView.Adapter<DoctorAdapter.ViewHolder>(){
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.textView.text = dataSet[position].name
+        dataSet[position].imageId?.let { holder.imageView.setImageResource(it) }
     }
 
     override fun getItemCount() = dataSet.size
