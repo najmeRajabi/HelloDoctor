cf9d2c831a7c31ca4a5805e13fe51fbc9dbc424c
niha8515 <niha8515@yahoo.com>
2022-04-21 11:25:18 +0430

    add DoctorAdapter.kt

---
+++ app/src/main/java/com/example/sampleproject/DoctorAdapter.kt
@@ -0,0 +1,7 @@
+package com.example.sampleproject
+
+import androidx.recyclerview.widget.RecyclerView
+
+class DoctorAdapter(private val datas :ArrayList<Doctor>) :
+RecyclerView.Adapter<DoctorAdapter.ViewHolder>{
+}
