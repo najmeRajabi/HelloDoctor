af1eea37b1541a576eaea76da06cb6aa7aa8c526
niha8515 <niha8515@yahoo.com>
2022-04-21 11:29:58 +0430

    add doctor_lisst_row.xml

--- app/src/main/java/com/example/sampleproject/DoctorAdapter.kt
+++ app/src/main/java/com/example/sampleproject/DoctorAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.sampleproject
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
@@ -17,8 +18,11 @@ RecyclerView.Adapter<DoctorAdapter.ViewHolder>(){
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.text_row_item, viewGroup, false)
+
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
