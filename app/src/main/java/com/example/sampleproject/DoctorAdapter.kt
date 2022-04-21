28cf3b539e2593ce1b4f2335e8820453d7c069ba
niha8515 <niha8515@yahoo.com>
2022-04-21 11:26:39 +0430

    add class ViewHolder in DoctorAdapter.kt

--- app/src/main/java/com/example/sampleproject/DoctorAdapter.kt
+++ app/src/main/java/com/example/sampleproject/DoctorAdapter.kt
@@ -1,7 +1,19 @@
 package com.example.sampleproject
 
+import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class DoctorAdapter(private val datas :ArrayList<Doctor>) :
 RecyclerView.Adapter<DoctorAdapter.ViewHolder>{
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+//        val textView: TextView
+
+        init {
+            // Define click listener for the ViewHolder's View.
+//            textView = view.findViewById(R.id.textView)
+        }
+    }
+
 }
