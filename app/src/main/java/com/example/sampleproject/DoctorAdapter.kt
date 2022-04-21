38b0674101e40dc531ab010cd88041171cc0d295
niha8515 <niha8515@yahoo.com>
2022-04-21 11:37:33 +0430

    update fun s in DoctorAdapter.kt

--- app/src/main/java/com/example/sampleproject/DoctorAdapter.kt
+++ app/src/main/java/com/example/sampleproject/DoctorAdapter.kt
@@ -3,17 +3,17 @@ package com.example.sampleproject
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class DoctorAdapter(private val datas :ArrayList<Doctor>) :
+class DoctorAdapter(private val dataSet :ArrayList<Doctor>) :
 RecyclerView.Adapter<DoctorAdapter.ViewHolder>(){
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-//        val textView: TextView
+        val textView: TextView
 
         init {
-            // Define click listener for the ViewHolder's View.
-//            textView = view.findViewById(R.id.textView)
+            textView = view.findViewById(R.id.txv_list_row)
         }
     }
 
@@ -25,11 +25,9 @@ RecyclerView.Adapter<DoctorAdapter.ViewHolder>(){
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.textView.text = dataSet[position].name
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount() = dataSet.size
 
 }
