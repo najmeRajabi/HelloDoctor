a4b4ec3bdae56090060e6c82d972fa2e3adbf739
niha8515 <niha8515@yahoo.com>
2022-04-21 11:27:35 +0430

    implement methode in DoctorAdapter.kt

--- app/src/main/java/com/example/sampleproject/DoctorAdapter.kt
+++ app/src/main/java/com/example/sampleproject/DoctorAdapter.kt
@@ -1,11 +1,12 @@
 package com.example.sampleproject
 
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class DoctorAdapter(private val datas :ArrayList<Doctor>) :
-RecyclerView.Adapter<DoctorAdapter.ViewHolder>{
+RecyclerView.Adapter<DoctorAdapter.ViewHolder>(){
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 //        val textView: TextView
@@ -14,6 +15,18 @@ RecyclerView.Adapter<DoctorAdapter.ViewHolder>{
             // Define click listener for the ViewHolder's View.
 //            textView = view.findViewById(R.id.textView)
         }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
     }
 
 }
